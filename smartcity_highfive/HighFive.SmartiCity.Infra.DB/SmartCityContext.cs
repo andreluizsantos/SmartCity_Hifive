0df9762f5d15262eed28f00bd2c46552bc838981
leandropessini <leandro.pessini@gmail.com>
2022-04-23 12:25:00 -0300

    Add files via upload

---
+++ smartcity_highfive/HighFive.SmartiCity.Infra.DB/SmartCityContext.cs
@@ -0,0 +1,24 @@
+﻿using HighFive.SmartCity.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace HighFive.SmartiCity.Infra.DB
+{
+    public class SmartCityContext : DbContext
+    {
+        public SmartCityContext(DbContextOptions<SmartCityContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<UsuarioPontosHistorico> UsuarioPontosHistorico { get; set; }
+        public DbSet<CategoriaOferta> CategoriaOferta { get; set; }
+        public DbSet<TipoMovimentacaoHistorico> TipoMovimentacaoHistorico { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<UsuarioCompraOferta> UsuarioCompraOferta { get; set; }
+        public DbSet<UsuarioOferta> UsuarioOferta { get; set; }
+        public DbSet<UsuarioOfertaDisponibilidade> UsuarioOfertaDisponibilidade { get; set; }
+        public DbSet<UsuarioOfertaAvaliacao> UsuarioOfertaAvaliacao { get; set; }
+
+
+    }
+}
